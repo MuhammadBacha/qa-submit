6c37d42fe2eb37fe8ef9ab76fcdb1f7ba1d663b6
muhammad <muhammadaljoufi@gmail.com>
2023-07-31 20:21:23 -0700

    made delete hook cleaner

--- src/hooks/useDeleteStudent.jsx
+++ src/hooks/useDeleteStudent.jsx
@@ -1,25 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteStudent } from "../services/helpers";
-import { useSearchParams } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
-function useDeleteStudent(numberOfPages) {
+function useDeleteStudent() {
   const queryClient = useQueryClient();
-
-  const [searchParams] = useSearchParams();
-  const page = +searchParams.get("page") || 0;
-  const sort = searchParams.get("sort") || "netMarks_dsc";
-
-  let stream = searchParams.get("stream");
-  stream = !stream || stream === "all" ? null : stream;
-
-  let test = searchParams.get("test");
-  test = !test || test === "all" ? null : test;
-
-  let desiredField = searchParams.get("desiredField");
-  desiredField = !desiredField || desiredField === "all" ? null : desiredField;
-
-  const filters = { stream, test, desiredField };
 
   const { isLoading, mutate } = useMutation({
     mutationFn: (id) => deleteStudent(id),
